806dc03a1c2f500c5cd6597e1b0308a278fa4006
Cayne <caynebyron@gmail.com>
2021-03-07 10:41:36 -0800

    Replaced grid view with staggered grid view

--- lib/widget/images_list_widget.dart
+++ lib/widget/images_list_widget.dart
@@ -7,6 +7,7 @@ import 'package:photo_manager/photo_manager.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:photo_gallery/modals/modal_fit.dart';
 import 'package:draggable_scrollbar/draggable_scrollbar.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class ImagesListWidget extends StatelessWidget {
   const ImagesListWidget({
@@ -23,6 +24,107 @@ class ImagesListWidget extends StatelessWidget {
   final List<AssetEntity> assetList;
   final scaffoldKey;
   final void Function(AssetPathEntity album) switchAlbum;
+
+  List<StaggeredTile> getStaggeredTiles() {
+    List<StaggeredTile> staggeredTiles = <StaggeredTile>[];
+    staggeredTiles.add(StaggeredTile.count(4, 0.5));
+    for (int i = 0; i < images.length; i++) {
+      staggeredTiles.add(StaggeredTile.count(2, 3));
+    }
+    return staggeredTiles;
+  }
+
+  List<Widget> getTiles(BuildContext context) {
+    List<Widget> prefix = <Widget>[];
+    prefix.add(Text(album.name,
+      style: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: Colors.black,
+          fontSize: 20.0
+      ),
+    ));
+    List<Widget> gallery = List.generate(images.length, (index) {
+      return GestureDetector(
+        onTap: () async {
+          AssetEntity entity = assetList[index];
+          if (entity.type == AssetType.image) {
+            Uint8List image = await entity.thumbData;
+            Navigator.pushNamed(context, '/view', arguments: {
+              'assetList': assetList,
+              'image': image,
+              'entity': entity,
+            });
+          } else if (entity.type == AssetType.video) {
+            Navigator.pushNamed(context, '/view_video', arguments: {
+              'entity': entity,
+            });
+          }
+        },
+        onLongPress: () async {
+          Uint8List image = await this.assetList[index].thumbData;
+          showMaterialModalBottomSheet(
+            expand: false,
+            context: context,
+            backgroundColor: Colors.transparent,
+            builder: (context) => ModalFit(
+              album: this.album,
+              switchAlbum: this.switchAlbum,
+              image: image,
+              asset: this.assetList[index],
+              scaffoldKey: this.scaffoldKey,
+            ),
+          );
+        },
+        child: Center(
+          child: AspectRatio(
+            aspectRatio: 487 / 700,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border.all(
+                  color: Colors.transparent,
+                  width: 0,
+                ),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(4),
+                  child: assetList[index].type == AssetType.image ? Image(
+                    image: MemoryImage(images[assetList[index].title]),
+                    fit: BoxFit.cover,
+                  ) : assetList[index].type == AssetType.video ? Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      Image(
+                        image: MemoryImage(images[assetList[index].title]),
+                        fit: BoxFit.cover,
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Icon(
+                          Icons.play_arrow_rounded,
+                          color: Colors.black,
+                          size: 38.0,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Icon(
+                          Icons.play_arrow_rounded,
+                          color: Colors.white,
+                          size: 36.0,
+                        ),
+                      ),
+                    ],
+                  ) : null
+              ),
+            ),
+          ),
+        ),
+      );
+    });
+    return List.from(prefix)..addAll(gallery);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,96 +151,108 @@ class ImagesListWidget extends StatelessWidget {
               padding: EdgeInsets.all(0),
               child: Card(
                 elevation: 5.0,
-                child: DraggableScrollbar.semicircle(
-                  controller: semicircleController,
-                  child: GridView.count(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DraggableScrollbar.semicircle(
                     controller: semicircleController,
-                    crossAxisCount: 2,
-                    childAspectRatio: 487 / 700,
-                    children: List.generate(images.length, (index) {
-                      return GestureDetector(
-                        onTap: () async {
-                          AssetEntity entity = assetList[index];
-                          if (entity.type == AssetType.image) {
-                            Uint8List image = await entity.thumbData;
-                            Navigator.pushNamed(context, '/view', arguments: {
-                              'assetList': assetList,
-                              'image': image,
-                              'entity': entity,
-                            });
-                          } else if (entity.type == AssetType.video) {
-                            Navigator.pushNamed(context, '/view_video', arguments: {
-                              'entity': entity,
-                            });
-                          }
-                        },
-                        onLongPress: () async {
-                          Uint8List image = await this.assetList[index].thumbData;
-                          showMaterialModalBottomSheet(
-                            expand: false,
-                            context: context,
-                            backgroundColor: Colors.transparent,
-                            builder: (context) => ModalFit(
-                              album: this.album,
-                              switchAlbum: this.switchAlbum,
-                              image: image,
-                              asset: this.assetList[index],
-                              scaffoldKey: this.scaffoldKey,
-                            ),
-                          );
-                        },
-                        child: Center(
-                          heightFactor: 350,
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(4, 0, 4, 0),
-                            child: AspectRatio(
-                              aspectRatio: 487 / 700,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.transparent,
-                                  border: Border.all(
-                                    color: Colors.transparent,
-                                    width: 0,
-                                  ),
-                                  borderRadius: BorderRadius.circular(4),
-                                ),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(4),
-                                  child: assetList[index].type == AssetType.image ? Image(
-                                      image: MemoryImage(images[assetList[index].title]),
-                                      fit: BoxFit.cover,
-                                  ) : assetList[index].type == AssetType.video ? Stack(
-                                    fit: StackFit.expand,
-                                    children: <Widget>[
-                                      Image(
-                                        image: MemoryImage(images[assetList[index].title]),
-                                        fit: BoxFit.cover,
-                                      ),
-                                      Align(
-                                        alignment: Alignment.center,
-                                        child: Icon(
-                                          Icons.play_arrow_rounded,
-                                          color: Colors.black,
-                                          size: 38.0,
-                                        ),
-                                      ),
-                                      Align(
-                                        alignment: Alignment.center,
-                                        child: Icon(
-                                          Icons.play_arrow_rounded,
-                                          color: Colors.white,
-                                          size: 36.0,
-                                        ),
-                                      ),
-                                    ],
-                                  ) : null
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
-                    }),
+                    child: StaggeredGridView.count(
+                      controller: semicircleController,
+                      crossAxisCount: 4,
+                      staggeredTiles: getStaggeredTiles(),
+                      mainAxisSpacing: 4.0,
+                      crossAxisSpacing: 8.0,
+                      padding: EdgeInsets.all(4.0),
+                      children: getTiles(context),
+                    )
+                    // child: GridView.count(
+                    //   controller: semicircleController,
+                    //   crossAxisCount: 2,
+                    //   childAspectRatio: 487 / 700,
+                    //   scrollDirection: Axis.vertical,
+                    //   mainAxisSpacing: 8.0,
+                    //   crossAxisSpacing: 8.0,
+                    //   children: List.generate(images.length, (index) {
+                    //     return GestureDetector(
+                    //       onTap: () async {
+                    //         AssetEntity entity = assetList[index];
+                    //         if (entity.type == AssetType.image) {
+                    //           Uint8List image = await entity.thumbData;
+                    //           Navigator.pushNamed(context, '/view', arguments: {
+                    //             'assetList': assetList,
+                    //             'image': image,
+                    //             'entity': entity,
+                    //           });
+                    //         } else if (entity.type == AssetType.video) {
+                    //           Navigator.pushNamed(context, '/view_video', arguments: {
+                    //             'entity': entity,
+                    //           });
+                    //         }
+                    //       },
+                    //       onLongPress: () async {
+                    //         Uint8List image = await this.assetList[index].thumbData;
+                    //         showMaterialModalBottomSheet(
+                    //           expand: false,
+                    //           context: context,
+                    //           backgroundColor: Colors.transparent,
+                    //           builder: (context) => ModalFit(
+                    //             album: this.album,
+                    //             switchAlbum: this.switchAlbum,
+                    //             image: image,
+                    //             asset: this.assetList[index],
+                    //             scaffoldKey: this.scaffoldKey,
+                    //           ),
+                    //         );
+                    //       },
+                    //       child: Center(
+                    //         heightFactor: 350,
+                    //         child: AspectRatio(
+                    //           aspectRatio: 487 / 700,
+                    //           child: Container(
+                    //             decoration: BoxDecoration(
+                    //               color: Colors.transparent,
+                    //               border: Border.all(
+                    //                 color: Colors.transparent,
+                    //                 width: 0,
+                    //               ),
+                    //               borderRadius: BorderRadius.circular(4),
+                    //             ),
+                    //             child: ClipRRect(
+                    //               borderRadius: BorderRadius.circular(4),
+                    //               child: assetList[index].type == AssetType.image ? Image(
+                    //                   image: MemoryImage(images[assetList[index].title]),
+                    //                   fit: BoxFit.cover,
+                    //               ) : assetList[index].type == AssetType.video ? Stack(
+                    //                 fit: StackFit.expand,
+                    //                 children: <Widget>[
+                    //                   Image(
+                    //                     image: MemoryImage(images[assetList[index].title]),
+                    //                     fit: BoxFit.cover,
+                    //                   ),
+                    //                   Align(
+                    //                     alignment: Alignment.center,
+                    //                     child: Icon(
+                    //                       Icons.play_arrow_rounded,
+                    //                       color: Colors.black,
+                    //                       size: 38.0,
+                    //                     ),
+                    //                   ),
+                    //                   Align(
+                    //                     alignment: Alignment.center,
+                    //                     child: Icon(
+                    //                       Icons.play_arrow_rounded,
+                    //                       color: Colors.white,
+                    //                       size: 36.0,
+                    //                     ),
+                    //                   ),
+                    //                 ],
+                    //               ) : null
+                    //             ),
+                    //           ),
+                    //         ),
+                    //       ),
+                    //     );
+                    //   }),
+                    // ),
                   ),
                 ),
               ),
