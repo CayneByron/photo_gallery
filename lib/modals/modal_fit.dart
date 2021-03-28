6cb03dbb00ba84bcfd4e7a2310aac82a5e36d1dd
Cayne <caynebyron@gmail.com>
2021-03-28 09:37:02 -0700

    Removed editing features to target api 30

--- lib/modals/modal_fit.dart
+++ lib/modals/modal_fit.dart
@@ -64,93 +64,93 @@ class ModalFit extends StatelessWidget {
                   }).then((value) => loadAll());
                 },
               ),
-              ListTile(
-                title: Text('Move'),
-                leading: Icon(Icons.content_copy),
-                onTap: () async {
-                  List<AssetPathEntity> albumList = await PhotoManager.getAssetPathList();
-                  albumList.sort((a, b) => a.name.compareTo(b.name));
-                  albumList.remove(album);
-                  AssetEntity entity = asset;
-                  Uint8List fullSizedImage = await entity.originBytes;
-                  File file = await entity.file;
-                  await Navigator.pushNamed(context, '/move', arguments: {
-                    'image': fullSizedImage,
-                    'file': file,
-                    'entity': entity,
-                    'albumList': albumList,
-                    'isMove': true,
-                  });
-                  loadAll();
-                  Navigator.of(context).pop();
-                }
-              ),
-              ListTile(
-                title: Text('Copy'),
-                leading: Icon(Icons.folder_open),
-                onTap: () async {
-                  List<AssetPathEntity> albumList = await PhotoManager.getAssetPathList();
-                  albumList.sort((a, b) => a.name.compareTo(b.name));
-                  albumList.remove(album);
-                  AssetEntity entity = asset;
-                  File file = await entity.file;
-                  Uint8List fullSizedImage = await entity.originBytes;
-                  await Navigator.pushNamed(context, '/move', arguments: {
-                    'image': fullSizedImage,
-                    'file': file,
-                    'entity': entity,
-                    'albumList': albumList,
-                    'isMove': false,
-                  });
-                  loadAll();
-                  Navigator.of(context).pop();
-                }
-              ),
-              Visibility(
-                visible: (asset.type == AssetType.image),
-                child: ListTile(
-                    title: Text('Crop'),
-                    leading: Icon(Icons.crop),
-                    onTap: () async {
-                      AssetEntity entity = asset;
-                      File imageFile = await entity.file;
-                      File croppedFile = await ImageCropper.cropImage(
-                          sourcePath: imageFile.path,
-                          aspectRatioPresets: Platform.isAndroid
-                              ? [
-                            CropAspectRatioPreset.square,
-                            CropAspectRatioPreset.ratio3x2,
-                            CropAspectRatioPreset.original,
-                            CropAspectRatioPreset.ratio4x3,
-                            CropAspectRatioPreset.ratio16x9
-                          ]
-                              : [
-                            CropAspectRatioPreset.original,
-                            CropAspectRatioPreset.square,
-                            CropAspectRatioPreset.ratio3x2,
-                            CropAspectRatioPreset.ratio4x3,
-                            CropAspectRatioPreset.ratio5x3,
-                            CropAspectRatioPreset.ratio5x4,
-                            CropAspectRatioPreset.ratio7x5,
-                            CropAspectRatioPreset.ratio16x9
-                          ],
-                          androidUiSettings: AndroidUiSettings(
-                              toolbarTitle: 'Cropper',
-                              toolbarColor: Colors.deepOrange,
-                              toolbarWidgetColor: Colors.white,
-                              initAspectRatio: CropAspectRatioPreset.original,
-                              lockAspectRatio: false),
-                          iosUiSettings: IOSUiSettings(
-                            title: 'Cropper',
-                          ));
-                      if (croppedFile != null) {
-                        imageFile.writeAsBytesSync(croppedFile.readAsBytesSync());
-                      }
-                      loadAll();
-                      Navigator.of(context).pop();
-                    }
-                ),
-              ),
+              // ListTile(
+              //   title: Text('Move'),
+              //   leading: Icon(Icons.content_copy),
+              //   onTap: () async {
+              //     List<AssetPathEntity> albumList = await PhotoManager.getAssetPathList();
+              //     albumList.sort((a, b) => a.name.compareTo(b.name));
+              //     albumList.remove(album);
+              //     AssetEntity entity = asset;
+              //     Uint8List fullSizedImage = await entity.originBytes;
+              //     File file = await entity.file;
+              //     await Navigator.pushNamed(context, '/move', arguments: {
+              //       'image': fullSizedImage,
+              //       'file': file,
+              //       'entity': entity,
+              //       'albumList': albumList,
+              //       'isMove': true,
+              //     });
+              //     loadAll();
+              //     Navigator.of(context).pop();
+              //   }
+              // ),
+              // ListTile(
+              //   title: Text('Copy'),
+              //   leading: Icon(Icons.folder_open),
+              //   onTap: () async {
+              //     List<AssetPathEntity> albumList = await PhotoManager.getAssetPathList();
+              //     albumList.sort((a, b) => a.name.compareTo(b.name));
+              //     albumList.remove(album);
+              //     AssetEntity entity = asset;
+              //     File file = await entity.file;
+              //     Uint8List fullSizedImage = await entity.originBytes;
+              //     await Navigator.pushNamed(context, '/move', arguments: {
+              //       'image': fullSizedImage,
+              //       'file': file,
+              //       'entity': entity,
+              //       'albumList': albumList,
+              //       'isMove': false,
+              //     });
+              //     loadAll();
+              //     Navigator.of(context).pop();
+              //   }
+              // ),
+              // Visibility(
+              //   visible: (asset.type == AssetType.image),
+              //   child: ListTile(
+              //       title: Text('Crop'),
+              //       leading: Icon(Icons.crop),
+              //       onTap: () async {
+              //         AssetEntity entity = asset;
+              //         File imageFile = await entity.file;
+              //         File croppedFile = await ImageCropper.cropImage(
+              //             sourcePath: imageFile.path,
+              //             aspectRatioPresets: Platform.isAndroid
+              //                 ? [
+              //               CropAspectRatioPreset.square,
+              //               CropAspectRatioPreset.ratio3x2,
+              //               CropAspectRatioPreset.original,
+              //               CropAspectRatioPreset.ratio4x3,
+              //               CropAspectRatioPreset.ratio16x9
+              //             ]
+              //                 : [
+              //               CropAspectRatioPreset.original,
+              //               CropAspectRatioPreset.square,
+              //               CropAspectRatioPreset.ratio3x2,
+              //               CropAspectRatioPreset.ratio4x3,
+              //               CropAspectRatioPreset.ratio5x3,
+              //               CropAspectRatioPreset.ratio5x4,
+              //               CropAspectRatioPreset.ratio7x5,
+              //               CropAspectRatioPreset.ratio16x9
+              //             ],
+              //             androidUiSettings: AndroidUiSettings(
+              //                 toolbarTitle: 'Cropper',
+              //                 toolbarColor: Colors.deepOrange,
+              //                 toolbarWidgetColor: Colors.white,
+              //                 initAspectRatio: CropAspectRatioPreset.original,
+              //                 lockAspectRatio: false),
+              //             iosUiSettings: IOSUiSettings(
+              //               title: 'Cropper',
+              //             ));
+              //         if (croppedFile != null) {
+              //           imageFile.writeAsBytesSync(croppedFile.readAsBytesSync());
+              //         }
+              //         loadAll();
+              //         Navigator.of(context).pop();
+              //       }
+              //   ),
+              // ),
               ListTile(
                 title: Text('Share'),
                 leading: Icon(Icons.share),
@@ -163,46 +163,46 @@ class ModalFit extends StatelessWidget {
                   Navigator.of(context).pop();
                 },
               ),
-              ListTile(
-                title: Text('Delete'),
-                leading: Icon(Icons.delete),
-                onTap: () async {
-                  Widget cancelButton = FlatButton(
-                    child: Text("Cancel"),
-                    onPressed:  () {
-                      loadAll();
-                      Navigator.of(context).pop();
-                      Navigator.of(context).pop();
-                    },
-                  );
-                  Widget continueButton = FlatButton(
-                    child: Text("Continue"),
-                    onPressed:  () async {
-                      AssetEntity entity = asset;
-                      File file = await entity.file;
-                      await file.delete();
-                      loadAll();
-                      Navigator.of(context).pop();
-                      Navigator.of(context).pop();
-                    },
-                  );
-                  AlertDialog alert = AlertDialog(
-                    title: Text("Delete"),
-                    content: Text("Are you sure you want to delete? Action cannot be undone."),
-                    actions: [
-                      cancelButton,
-                      continueButton,
-                    ],
-                  );
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return alert;
-                    },
-                  );
-                }
-                // onTap: () => Navigator.of(context).pop(),
-              ),
+              // ListTile(
+              //   title: Text('Delete'),
+              //   leading: Icon(Icons.delete),
+              //   onTap: () async {
+              //     Widget cancelButton = FlatButton(
+              //       child: Text("Cancel"),
+              //       onPressed:  () {
+              //         loadAll();
+              //         Navigator.of(context).pop();
+              //         Navigator.of(context).pop();
+              //       },
+              //     );
+              //     Widget continueButton = FlatButton(
+              //       child: Text("Continue"),
+              //       onPressed:  () async {
+              //         AssetEntity entity = asset;
+              //         File file = await entity.file;
+              //         await file.delete();
+              //         loadAll();
+              //         Navigator.of(context).pop();
+              //         Navigator.of(context).pop();
+              //       },
+              //     );
+              //     AlertDialog alert = AlertDialog(
+              //       title: Text("Delete"),
+              //       content: Text("Are you sure you want to delete? Action cannot be undone."),
+              //       actions: [
+              //         cancelButton,
+              //         continueButton,
+              //       ],
+              //     );
+              //     showDialog(
+              //       context: context,
+              //       builder: (BuildContext context) {
+              //         return alert;
+              //       },
+              //     );
+              //   }
+              //   // onTap: () => Navigator.of(context).pop(),
+              // ),
             ],
           ),
         ));
